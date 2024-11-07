8610a921dfec487220a3884bad60f31826eee52d
KatyPotatto <kareenc3012@gmail.com>
2024-11-07 14:23:15 -0600

    Nuevos Assets
    Sonidos de carro y Explosiones

--- FinalProyect/Assets/Scripts/MenuPause.cs
+++ FinalProyect/Assets/Scripts/MenuPause.cs
@@ -1,19 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MenuPause : MonoBehaviour
 {
     [SerializeField] private GameObject buttonPause;
     [SerializeField] private GameObject menuPause;
+
+    private bool gamePause = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (gamePause)
+            {
+                Resume();
+            }
+            else { 
+                Pause();
+            }
+        }
+    }
     public void Pause() { 
         Time.timeScale = 0f;
+        buttonPause.SetActive(false);
+        menuPause.SetActive(true);
+        gamePause = true;
         Debug.Log("Pausaaaaa");
     }
     public void Resume() {
         Time.timeScale = 1f;
         buttonPause.SetActive(true);
         menuPause.SetActive(false);
+        gamePause = false;
         Debug.Log("Resumen");
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gamePause = false;
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Cerrar juego");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        gamePause = false;
     }
 }
