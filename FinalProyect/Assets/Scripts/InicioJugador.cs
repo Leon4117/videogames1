28b1ad5a4ce26bca35235647c9206b138636e722
KatyPotatto <kareenc3012@gmail.com>
2024-11-15 00:21:31 -0600

    Luces
    Se agregaron luces, pero el metodo de apagar luces de la calle no funciona.

--- FinalProyect/Assets/Scripts/InicioJugador.cs
+++ FinalProyect/Assets/Scripts/InicioJugador.cs
@@ -11,7 +11,7 @@ public class InicioJugador : MonoBehaviour
         int indexCarro = PlayerPrefs.GetInt("SeleccionIndex");
         //la rotación en la que lo quiero
         Quaternion rotacion = Quaternion.Euler(0, 90, 0);
-        //Vector3 posicion = new Vector3(105, 70, 10);
+        //la posicion que toma es la del objeto InicioJugador
         //intanciar el personaje, esta en game manager lista  le damos el index tomamos al prefab
         Instantiate(GameManager.Instance.seleccionsCar[indexCarro].carroPosible, transform.position, rotacion);
         //nuevo clima
--- FinalProyect/Assets/Scripts/Weather.cs
+++ FinalProyect/Assets/Scripts/Weather.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class Weather : MonoBehaviour
 {
@@ -18,6 +19,22 @@ public class Weather : MonoBehaviour
     public Color cloudyLi = new Color(0.5f, 0.5f, 0.6f);
     public Color sunnyLi = Color.white;
     public Color nightLi = new Color(0.2f, 0.2f, 0.3f);
+    //luces del carro
+    public Light[] carroLuces;
+    public Light[] farosLuces;
+    //luces de la calle
+    public Light[] calleLuces;
+
+    public void Start()
+    {
+        //asignar las luces de los faros
+        GameObject[] calleLucesObject = GameObject.FindGameObjectsWithTag("FaroLuz");
+        calleLuces = new Light[calleLucesObject.Length];
+        for (int i = 0;i < calleLucesObject.Length; i++)
+        {
+            calleLuces[i] = calleLucesObject[i].GetComponent<Light>();
+        }
+    }
     public void ChangeSkybox(int weatherType)
     {
         switch (weatherType)
@@ -25,25 +42,35 @@ public class Weather : MonoBehaviour
             case 1:
                 //se asigna el material al skybox
                 Debug.Log("Nublado");
+                PrenderLuces(false);
                 //asigna cierta intensidad a las luces junto con el color
                 mainLight.intensity = 0.8f;
                 mainLight.color = cloudyLi;
                 if (secLight != null) secLight.intensity = 0.3f;
+                LucesCarro(1.0f, false);
                 RenderSettings.skybox = cloudy;
+
                 break;
             case 2:
                 Debug.Log("Soleado");
+                PrenderLuces(false);
                 mainLight.intensity = 0.9f;
                 mainLight.color = sunnyLi;
                 if (secLight != null) secLight.intensity = 0.9f;
                 if (terLight != null) terLight.intensity = 0.3f;
+                LucesCarro(0.7f, false);
                 RenderSettings.skybox = sunny;
+
                 break;
             case 3:
                 Debug.Log("Noshe");
-                mainLight.intensity = 0.8f;
+                PrenderLuces(true);
+                mainLight.intensity = 0.5f;
                 mainLight.color = nightLi;
-                if (secLight != null) secLight.intensity = 0.5f;
+                if (secLight != null) secLight.intensity = 0.3f;
+                terLight.intensity = 0.2f;
+                LucesCarro(1.5f,true);
+                
                 RenderSettings.skybox = night;
                 break;
             default:
@@ -54,4 +81,29 @@ public class Weather : MonoBehaviour
         //Actualizar el clima
         DynamicGI.UpdateEnvironment();
     }
+    public void LucesCarro(float intensidad)
+    {
+        foreach (var light in carroLuces) { 
+            light.intensity = intensidad;
+        }
+    }
+    public void LucesCarro(float intensidad, bool faros)
+    {
+        foreach (var light in carroLuces)
+        {
+            light.intensity = intensidad;
+        }
+        foreach (var light in farosLuces)
+        {
+            light.enabled = faros;
+        }
+    }
+    public void PrenderLuces(bool estado)
+    {
+        foreach (var light in calleLuces)
+        {
+            light.enabled = estado;
+        }
+    }
+
 }
