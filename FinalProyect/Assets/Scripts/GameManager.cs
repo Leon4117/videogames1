2d8bedd60df23a5d726134f40fb4c559ce7b7433
Leonardo Aguilar <ld.aghdz@gmail.com>
2024-12-03 08:09:34 -0600

    Avance menú de selección
    avances para hacer un merge

--- FinalProyect/Assets/Scripts/GameManager.cs
+++ FinalProyect/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -25,7 +26,7 @@ public class GameManager : MonoBehaviour
     }
     void Start()
     {
-        
+        SceneManager.LoadScene("Menu");
     }
 
     // Update is called once per frame
